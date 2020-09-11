ef5504d7423e1ac3c499c06a1b0f2b850c6bd834
Navid Faradjzadeh <navid.faradjzadeh@ericsson.com>
2020-09-11 17:45:38 +0200

    first files

---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"math"
+	"math/rand"
+	"bufio"
+	"os"
+)
+
+import "matrix"
+
+type Game struct {
+	grid Matrix
+	reader	 *bufio.Reader
+}
+
+func (game *Game) init(i, j int) {
+	game.grid.init(i, j)
+	zeros,_ := game.grid.getZeros()
+	game.grid.set(zeros[rand.Intn(len(zeros))], rand2or4())
+	zeros,_ = game.grid.getZeros()
+	game.grid.set(zeros[rand.Intn(len(zeros))], rand2or4())
+	game.reader = bufio.NewReader(os.Stdin)
+}
+
+func (game *Game) actOnInput() {
+	command,_,_ := game.reader.ReadRune()
+	for !game.grid.shift(command) {
+		fmt.Println("Can't make that move! Try again!")
+		_,_,_ = game.reader.ReadRune()
+		_,_,_ = game.reader.ReadRune()
+		command,_,_ = game.reader.ReadRune()
+	}
+}
+
+func (game *Game) generateNew() bool {
+	zeros, err := game.grid.getZeros()
+	if !err {
+		return err
+	}
+	game.grid.set(zeros[rand.Intn(len(zeros))], rand2or4())
+	zeros,_ = game.grid.getZeros()
+	fmt.Println(zeros)
+	if err {
+		game.grid.set(zeros[rand.Intn(len(zeros))], rand2or4())
+	}
+	return true
+}
+
+func (game *Game) over() bool {
+	_, err := game.grid.getZeros()
+	return !err
+}
+
+func rand2or4() int {
+	return 2*(rand.Intn(2)+1)
+}
+
+func main() {
+	game := Game{}
+	game.init(3, 3)
+	fmt.Println(game.grid)
+	for !game.over() {
+		game.actOnInput()
+		_,_,_ = game.reader.ReadRune()
+		_,_,_ = game.reader.ReadRune()
+		game.generateNew()
+		fmt.Println(game.grid)
+	}
+}
---
+++ matrix.go
@@ -0,0 +1,128 @@
+package matrix
+
+type Matrix struct {
+	i, j int
+	data map[[2]int]int
+}
+
+func (matrix *Matrix) init(i, j int) {
+	matrix.i = i
+	matrix.j = j
+	matrix.data = make(map[[2]int]int)
+	for x:=0; x<matrix.i; x++ {
+		for y:=0; y<matrix.j; y++ {
+			matrix.data[[2]int{x, y}] = 0
+		}
+	}
+}
+
+func (matrix Matrix) String() string{
+	str := ""
+	for i:=0; i<matrix.i; i++ {
+		str += fmt.Sprintf("\n")
+		str += fmt.Sprintf("| ")
+		for j:=0; j<matrix.j; j++ {
+			str += fmt.Sprintf(strconv.Itoa(matrix.data[[2]int{i, j}]))
+			str += fmt.Sprintf(" | ")
+		}
+	}
+	return str
+}
+
+func (matrix *Matrix) shift(direction rune) bool{
+	type LoopParams struct {
+		low, high, step int
+	}
+	paramsA := LoopParams{}
+	paramsB := LoopParams{}
+	straight := true
+	err := false
+	
+	switch direction {
+		case 'r':
+			paramsA.low = 0
+			paramsA.high = matrix.i
+			paramsA.step = 1
+			paramsB.low = matrix.j-1
+			paramsB.high = 0
+			paramsB.step = -1
+		case 'l':
+			paramsA.low = 0
+			paramsA.high = matrix.i
+			paramsA.step = 1
+			paramsB.low = 0
+			paramsB.high = matrix.j-1
+			paramsB.step = 1
+		case 'd':
+			paramsA.low = 0
+			paramsA.high = matrix.j
+			paramsA.step = 1
+			paramsB.low = matrix.i-1
+			paramsB.high = 0
+			paramsB.step = -1
+			straight = false
+		case 'u':
+			paramsA.low = 0
+			paramsA.high = matrix.j
+			paramsA.step = 1
+			paramsB.low = 0
+			paramsB.high = matrix.i-1
+			paramsB.step = 1
+			straight = false
+		default:
+			return err
+	}
+
+	index := func(i, j int) [2]int {
+		if straight {
+			return [2]int{i, j}
+		}
+		return [2]int{j, i}
+	}
+
+	for i:=paramsA.low; i!=paramsA.high; i+=paramsA.step {
+		for j:=paramsB.low; j!=paramsB.high; j+=paramsB.step {
+			if matrix.data[index(i, j)] == matrix.data[index(i, j+paramsB.step)] {
+				matrix.data[index(i, j)] = matrix.data[index(i, j)] + matrix.data[index(i, j+paramsB.step)]
+				matrix.data[index(i, j+paramsB.step)] = 0
+			}
+		}
+	}
+	for round:=0; round<int(math.Abs(float64(paramsB.high-paramsB.low))); round++ {
+		for i:=paramsA.low; i!=paramsA.high; i+=paramsA.step {
+			for j:=paramsB.high; j!=paramsB.low; j-=paramsB.step {
+				if matrix.data[index(i, j-paramsB.step)] == 0{
+					matrix.data[index(i, j-paramsB.step)] = matrix.data[index(i, j)]
+					matrix.data[index(i, j)] = 0
+					err = true
+				}
+			}
+		}
+	}
+	return err
+}
+
+func (matrix *Matrix) get(coordinates [2]int) (int, bool) {
+	v, e := matrix.data[coordinates]
+	return v, e
+}
+
+func (matrix *Matrix) set(coordinates [2]int, v int) bool {
+	if _,err := matrix.get(coordinates); !err {
+		return err
+	}
+	matrix.data[coordinates] = v
+	return true
+}
+
+func (matrix *Matrix) getZeros() ([][2]int, bool) {
+	err := false
+	zeros := [][2]int{}
+	for k, v := range matrix.data {
+		if v == 0 {
+			zeros = append(zeros, k)
+			err = true
+		}
+	}
+	return zeros, err
+}

7ff811f908f1b65bd0d27b4a59cafdfd5f550e16
Navid Faradjzadeh <navid.faradjzadeh@ericsson.com>
2020-09-17 16:32:10 +0200

    Add Test for Game

--- game/game.go
+++ game/game.go
@@ -2,7 +2,6 @@ package game
 
 import (
 	"math/rand"
-	"fmt"
 )
 
 import "github.com/navafafa/2048/matrix"
@@ -29,21 +28,42 @@ func (game *Game) ActOnInput(command rune) bool {
 
 func (game *Game) GenerateNew() bool {
 	zeros, err := game.grid.GetZeros()
-	if !err {
-		return err
+	if err {
+		return true
 	}
 	game.grid.Set(zeros[rand.Intn(len(zeros))], rand2or4())
-	zeros,_ = game.grid.GetZeros()
-	fmt.Println(zeros)
-	if err {
+	zeros, err = game.grid.GetZeros()
+	if !err {
 		game.grid.Set(zeros[rand.Intn(len(zeros))], rand2or4())
 	}
-	return true
+	return false
 }
 
 func (game *Game) Over() bool {
-	_, err := game.grid.GetZeros()
-	return !err
+	if _, noZeros := game.grid.GetZeros(); !noZeros {
+		return false
+	}
+	
+	for i:=0; i<game.grid.GetX(); i++ {
+		for j:=0; j<game.grid.GetY(); j++ {
+			if i != game.grid.GetX()-1 {
+				v1,_ := game.grid.Get([2]int{i, j})
+				v2,_ := game.grid.Get([2]int{i+1, j})
+				if v1 == v2 {
+					return false
+				}
+			}
+			if j != game.grid.GetY()-1 {
+				v1,_ := game.grid.Get([2]int{i, j})
+				v2,_ := game.grid.Get([2]int{i, j+1})
+				if v1 == v2 {
+					return false
+				}
+			}
+		}
+	}
+	
+	return true
 }
 
 func rand2or4() int {
--- game/game_test.go
+++ game/game_test.go
@@ -1,2 +1,47 @@
 package game
 
+import "testing"
+import "math/rand"
+import "fmt"
+
+var game Game = Game{}
+const (x=3; y=3)
+
+func setup(t *testing.T) {
+	game.Init(x, y)
+	
+	if got, _ := game.grid.GetZeros(); len(got) != x*y-2 {
+		t.Errorf("In Init() for the number of non zero elements got: %d want: 2", len(got))
+	}
+}
+
+func generateNew(t *testing.T) {
+	game.Init(x, y)
+
+	for err := false; !err; err = game.GenerateNew() {
+		game.ActOnInput('r')
+	}
+	if got, err := game.grid.GetZeros(); !err {
+		t.Errorf("In GenerateNew() the function returned while there are still %d zeros left", len(got))
+	}
+}
+
+func over(t *testing.T) {
+	for game.Init(x, y); !game.Over(); game.GenerateNew() {
+		fmt.Println(game)
+		moves := [4]rune{'r', 'l', 'u', 'd'}
+		move := moves[rand.Intn(len(moves))]
+		game.ActOnInput(move)
+		fmt.Println(string(move))
+	}
+	fmt.Println(game)
+	if game.ActOnInput('r') == true || game.ActOnInput('l') == true || game.ActOnInput('u') == true || game.ActOnInput('d') == true {
+		t.Errorf("In Over() the function returned true while there are still possible moves")
+	}
+}
+
+func TestGame(t *testing.T) {
+	t.Run("Setup", func(t *testing.T){setup(t)})
+	t.Run("GenerateNew", func(t *testing.T){generateNew(t)})
+	t.Run("Over", func(t *testing.T){over(t)})
+}
